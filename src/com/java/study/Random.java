d8383339010adc73f4ccdab0c9db859c887c3d4f
yzh <1213790892@qq.com>
2020-01-14 23:29:56 +0800

    随机数更新优化

--- src/com/java/study/Random.java
+++ src/com/java/study/Random.java
@@ -1,8 +1,7 @@
 package com.java.study;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
 
 
 /**
@@ -14,7 +13,6 @@ import java.io.IOException;
  **/
 public class Random {
     public static void main(String[] args) throws IOException {
-
         int[] rand = new int[100];
         for (int i = 0; i <= 99; i++) {
             int number = (int) (Math.random() * 100);
@@ -22,18 +20,9 @@ public class Random {
         }
         //创建文件夹，将存放随机数的txt文件放入
         File file = new File("E:/vacation-work/Random.txt");
-        //判断是否有这个文件夹，若没有就创建
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Random.txt文件创建成功");
-        } else {
-            FileWriter randoms = new FileWriter(file);
-            for (int i=0;i<99;i++)
-            randoms.write(String.valueOf(rand[i])+" ");
+        Writer writer = new FileWriter(file);
+        PrintWriter randoms = new PrintWriter(writer);
+            randoms.print(Arrays.toString(rand));
             randoms.close();
             System.out.println("随机数生成成功");
         }
@@ -41,4 +30,4 @@ public class Random {
 
 
 
-}
+
